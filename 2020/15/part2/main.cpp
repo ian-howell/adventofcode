a89b0c2c9f9484f238c16f147e1bf0aec8f5a707
Ian Howell <ian.howell0@gmail.com>
2020-12-15 09:31:00 -0600

    2020 day15 part2
    This runs on the input in 13s. I think I'd need a better map than
    std::unordered_map to do any better

---
+++ 2020/15/part2/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <unordered_map>
+#include <string>
+#include <vector>
+
+std::vector<int> get_initial_nums();
+std::vector<std::string> split(const std::string& s, char delim);
+
+int main() {
+    auto initial_nums = get_initial_nums();
+    std::unordered_map<int, int> last_time_spoken;
+    last_time_spoken.reserve(30000000);
+
+    for (size_t i = 0; i < initial_nums.size() - 1; i++) {
+        last_time_spoken[initial_nums.at(i)] = i;
+    }
+
+    int last_spoken_num = initial_nums.at(initial_nums.size() - 1);
+    for (int current_turn = initial_nums.size(); current_turn < 30000000; current_turn++) {
+        if (last_time_spoken.find(last_spoken_num) == last_time_spoken.end()) {
+            last_time_spoken[last_spoken_num] = current_turn - 1;
+            last_spoken_num = 0;
+        } else {
+            int diff = current_turn - last_time_spoken.at(last_spoken_num) - 1;
+            last_time_spoken[last_spoken_num] = current_turn - 1;
+            last_spoken_num = diff;
+        }
+    }
+    std::cout << last_spoken_num << std::endl;
+
+    return 0;
+}
+
+std::vector<std::string> split(const std::string& s, char delim) {
+    size_t lb = 0;
+    size_t rb;
+    std::vector<std::string> splits;
+    do {
+        rb = s.find(delim, lb);
+        splits.push_back(s.substr(lb, (rb - lb)));
+        lb = rb + 1;
+    } while (rb != std::string::npos);
+    return splits;
+}
+
+std::vector<int> get_initial_nums() {
+    std::string line;
+    std::cin >> line;
+    std::vector<int> nums;
+    for (const auto& it : split(line, ',')) {
+        nums.push_back(std::stoi(it));
+    }
+    return nums;
+}
