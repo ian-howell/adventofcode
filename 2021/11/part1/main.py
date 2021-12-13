a1268eb0df25406341cb57a4fbd0a51be85da3f2
Ian Howell <ian.howell0@gmail.com>
2021-12-13 13:35:38 -0600

    2021 day11 part1

---
+++ 2021/11/part1/main.py
@@ -0,0 +1,62 @@
+import sys
+
+
+def main():
+    flashes = 0
+    grid = []
+    for line in sys.stdin.readlines():
+        row = []
+        for val in line.strip():
+            row.append(int(val))
+        grid.append(row)
+
+    for i in range(100):
+        flashes += step(grid)
+    print(flashes)
+
+def step(grid):
+    flashes = 0
+    increment(grid)
+    for r, row in enumerate(grid):
+        for c in range(len(row)):
+            if grid[r][c] == 10:
+                flashes += propogate(grid, r, c)
+    reset(grid)
+    return flashes
+
+
+def increment(grid):
+    for r, row in enumerate(grid):
+        for c in range(len(row)):
+            grid[r][c] += 1
+
+
+def propogate(grid, r, c):
+    flashes = 1
+    grid[r][c] = 11
+    for neighbor in get_neighbors(grid, r, c):
+        new_r, new_c = neighbor
+        if grid[new_r][new_c] <= 9:
+            grid[new_r][new_c] += 1
+        if grid[new_r][new_c] == 10:
+            flashes += propogate(grid, new_r, new_c)
+    return flashes
+
+
+def reset(grid):
+    for r, row in enumerate(grid):
+        for c in range(len(row)):
+            if grid[r][c] >= 10:
+                grid[r][c] = 0
+
+
+def get_neighbors(grid, r, c):
+    neighbors = []
+    for m in ((+0, +1), (+0, -1), (+1, +0), (-1, +0), (+1, +1), (-1, -1), (+1, -1), (-1, +1)):
+        new_r, new_c = r+m[0], c+m[1]
+        if (0 <= new_r < len(grid)) and (0 <= new_c < len(grid[0])) and (grid[new_r][new_c] <= 9):
+            neighbors.append((new_r, new_c))
+    return neighbors
+
+
+main()
