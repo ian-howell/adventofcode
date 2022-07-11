ae40844f31c353bd3f900b372c2366cbc1ebeee5
Ian Howell <ian.howell0@gmail.com>
2022-07-11 10:36:33 -0500

    2015 day10 part2

---
+++ 2015/10/part2/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	var input string
+	fmt.Scan(&input)
+	for i := 0; i < 50; i++ {
+		input = step(input)
+	}
+	fmt.Println(len(input))
+}
+
+func step(s string) string {
+	l := len(s)
+	if l == 1 {
+		return fmt.Sprintf("1%v", s[0]-'0')
+	}
+	result := strings.Builder{}
+	i := 0
+	for j := 1; j < len(s); j++ {
+		if s[i] != s[j] {
+			result.WriteString(fmt.Sprintf("%v%v", j-i, s[i]-'0'))
+			i = j
+		}
+	}
+	result.WriteString(fmt.Sprintf("%v%v", l-i, s[l-1]-'0'))
+	return result.String()
+}
