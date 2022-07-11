d4fcf3ea1d6de77433caf168d848c54faef60b82
Ian Howell <ian.howell0@gmail.com>
2022-07-11 14:03:43 -0500

    2015 day13 part2

---
+++ 2015/13/part2/main.go
@@ -0,0 +1,64 @@
+package main
+
+import "fmt"
+
+func main() {
+	graph := readGraph()
+	addMe(graph)
+
+	guestList := []string{}
+	for guest := range graph {
+		guestList = append(guestList, guest)
+	}
+
+	best := evaluateSeatingChart(guestList, graph)
+	allGuestLists := generateAllLists(guestList)
+
+	for _, list := range allGuestLists {
+		current := evaluateSeatingChart(list, graph)
+		if current > best {
+			best = current
+		}
+	}
+	fmt.Println(best)
+}
+
+func evaluateSeatingChart(guestList []string, graph Graph) int {
+	l := len(guestList)
+	total := graph[guestList[0]][guestList[l-1]] + graph[guestList[l-1]][guestList[0]]
+	for i := 1; i < l; i++ {
+		total += graph[guestList[i]][guestList[i-1]] + graph[guestList[i-1]][guestList[i]]
+	}
+	return total
+}
+
+func generateAllLists(ss []string) [][]string {
+	if len(ss) == 1 {
+		return [][]string{{ss[0]}}
+	}
+
+	result := [][]string{}
+	for _, s := range ss {
+		for _, subList := range generateAllLists(listSubtract(ss, s)) {
+			result = append(result, append([]string{s}, subList...))
+		}
+	}
+	return result
+}
+
+func listSubtract(ss []string, s string) []string {
+	newList := []string{}
+	for _, v := range ss {
+		if v != s {
+			newList = append(newList, v)
+		}
+	}
+	return newList
+}
+
+func addMe(graph Graph) {
+	for guest := range graph {
+		graph.addEdge("me", guest, 0)
+		graph.addEdge(guest, "me", 0)
+	}
+}
