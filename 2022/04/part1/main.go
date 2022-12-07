a6fa26bdacbbed4ced598ccedacf4b88f1534fa4
Ian Howell <ian.howell0@gmail.com>
2022-12-07 09:16:35 -0600

    2022 day04 part1

---
+++ 2022/04/part1/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	total := 0
+	s := bufio.NewScanner(os.Stdin)
+	for s.Scan() {
+		p1, p2 := getPairs(s.Text())
+		if p1.Contains(p2) || p2.Contains(p1) {
+			total++
+		}
+	}
+	fmt.Println(total)
+}
+
+type Pair struct {
+	Lower int
+	Upper int
+}
+
+func NewPair(s string) Pair {
+	parts := strings.Split(s, "-")
+	return Pair{
+		Lower: atoi(parts[0]),
+		Upper: atoi(parts[1]),
+	}
+}
+
+func (p Pair) Contains(o Pair) bool {
+	return p.Lower <= o.Lower && p.Upper >= o.Upper
+}
+
+func getPairs(s string) (Pair, Pair) {
+	parts := strings.Split(s, ",")
+	return NewPair(parts[0]), NewPair(parts[1])
+}
+
+func atoi(a string) int {
+	i, _ := strconv.Atoi(a)
+	return i
+}
