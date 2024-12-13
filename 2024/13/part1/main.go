8bfccb61431fdaf748455b4a1229a78fa4bb372e
Ian Howell <ian.howell0@gmail.com>
2024-12-13 07:37:17 -0600

    2024 day13 part1

---
+++ 2024/13/part1/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type ClawMachine struct {
+	ax, ay, bx, by, prizeX, prizeY int
+}
+
+func main() {
+	total := 0
+	clawMachines := getInput()
+	for _, clawMachine := range clawMachines {
+		aPresses, bPresses, exists := Solve(
+			LinearEquation{
+				a: float64(clawMachine.ax),
+				b: float64(clawMachine.bx),
+				s: float64(clawMachine.prizeX),
+			},
+			LinearEquation{
+
+				a: float64(clawMachine.ay),
+				b: float64(clawMachine.by),
+				s: float64(clawMachine.prizeY),
+			},
+		)
+
+		if exists {
+			total += int(aPresses)*3 + int(bPresses)
+		}
+	}
+
+	fmt.Println(total)
+}
+
+func getInput() []ClawMachine {
+	clawMachines := []ClawMachine{}
+	s := bufio.NewScanner(os.Stdin)
+	for s.Scan() {
+		ax, ay := parseButton(s.Text())
+		s.Scan()
+		bx, by := parseButton(s.Text())
+		s.Scan()
+		px, py := parsePosition(s.Text())
+		s.Scan()
+		clawMachines = append(
+			clawMachines,
+			ClawMachine{ax, ay, bx, by, px, py},
+		)
+	}
+	return clawMachines
+}
+
+func parseButton(s string) (int, int) {
+	s = strings.ReplaceAll(s, ",", "")
+	fields := strings.Fields(s)
+	xStr, yStr := fields[2], fields[3]
+	_, xStr, _ = strings.Cut(xStr, "+")
+	_, yStr, _ = strings.Cut(yStr, "+")
+	return atoi(xStr), atoi(yStr)
+}
+
+func parsePosition(s string) (int, int) {
+	s = strings.ReplaceAll(s, ",", "")
+	fields := strings.Fields(s)
+	xStr, yStr := fields[1], fields[2]
+	_, xStr, _ = strings.Cut(xStr, "=")
+	_, yStr, _ = strings.Cut(yStr, "=")
+	return atoi(xStr), atoi(yStr)
+}
+
+func atoi(a string) int {
+	i, _ := strconv.Atoi(a)
+	return i
+}
