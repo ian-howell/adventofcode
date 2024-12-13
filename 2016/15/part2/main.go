800ba2f18fb3f161b3c9e5d9e5ce098b538c141c
Ian Howell <ian.howell0@gmail.com>
2024-12-12 21:09:43 -0600

    2016 day15 part2

---
+++ 2016/15/part2/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Disc struct {
+	CurrentPosition int
+	NumPositions    int
+}
+
+func (d *Disc) Rotate() {
+	d.CurrentPosition = (d.CurrentPosition + 1) % d.NumPositions
+}
+
+func main() {
+	discs := getDiscs()
+	discs = append(discs, Disc{CurrentPosition: 0, NumPositions: 11})
+	t := 0
+	for !aligned(discs) {
+		t++
+		rotate(discs)
+	}
+	fmt.Println(t)
+}
+
+func rotate(discs []Disc) {
+	for i := range discs {
+		discs[i].Rotate()
+	}
+}
+
+func aligned(discs []Disc) bool {
+	for i, disc := range discs {
+		if (disc.CurrentPosition+i+1)%disc.NumPositions != 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func getDiscs() []Disc {
+	discs := []Disc{}
+	s := bufio.NewScanner(os.Stdin)
+	for s.Scan() {
+		discs = append(discs, toDisc(s.Text()))
+	}
+	return discs
+}
+
+func toDisc(s string) Disc {
+	segments := strings.Fields(s)
+	return Disc{
+		CurrentPosition: atoi(strings.Trim(segments[11], ".")),
+		NumPositions:    atoi(segments[3]),
+	}
+}
+
+func atoi(a string) int {
+	i, _ := strconv.Atoi(a)
+	return i
+}
